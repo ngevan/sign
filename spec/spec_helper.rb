4a56afa8c1ad3a2af46a0ea5c3f66a10f8dfb8e9
Evan Ng <dijkstrascloud@gmail.com>
2017-10-28 14:13:34 -0400

    Create TestHelper module to return capture cli output

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,21 @@
 require "bundler/setup"
 require "sign"
+require "test_helper"
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
+  config.include TestHelper
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
+  end
+
+  def fixture_path
+    File.expand_path("../fixtures", __FILE__)
+  end
+
+  def fixture(file)
+    File.new(fixture_path + "/" + file)
   end
 end
